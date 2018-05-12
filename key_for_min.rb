bad0d168449b112f72d030664e8d418e280c3b81
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-12 21:43:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,9 @@ def key_for_min_value(name_hash)
 k = ""
 v = 10000
 name_hash.each do |key, value|
-  if value < v
+  next if value < v
     v = value
     k = key
-  elsif 
-    next
 else 
   return nil
 return k
